a10ed2d0ef82c519035425ba60d32cbcbef51485
Chemsorly <Contact@chemsorly.com>
2016-12-02 12:22:56 +0100

    added 2010 project

---
+++ VisioAddin2010/MainRibbon.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace VisioAddin2010
+{
+    partial class MainRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public MainRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.ModelTargetDropDown = this.Factory.CreateRibbonDropDown();
+            this.VerifyButton = this.Factory.CreateRibbonButton();
+            this.ImportButton = this.Factory.CreateRibbonButton();
+            this.ExportButton = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.ModelTargetDropDown);
+            this.group1.Items.Add(this.VerifyButton);
+            this.group1.Items.Add(this.ImportButton);
+            this.group1.Items.Add(this.ExportButton);
+            this.group1.Label = "SPES";
+            this.group1.Name = "group1";
+            // 
+            // ModelTargetDropDown
+            // 
+            this.ModelTargetDropDown.Label = "dropDown1";
+            this.ModelTargetDropDown.Name = "ModelTargetDropDown";
+            this.ModelTargetDropDown.ShowLabel = false;
+            this.ModelTargetDropDown.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ModelTargetDropDown_SelectionChanged);
+            // 
+            // VerifyButton
+            // 
+            this.VerifyButton.Label = "Verify";
+            this.VerifyButton.Name = "VerifyButton";
+            this.VerifyButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.VerifyButton_Click);
+            // 
+            // ImportButton
+            // 
+            this.ImportButton.Label = "Import";
+            this.ImportButton.Name = "ImportButton";
+            this.ImportButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ImportButton_Click);
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Label = "Export";
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ExportButton_Click);
+            // 
+            // MainRibbon
+            // 
+            this.Name = "MainRibbon";
+            this.RibbonType = "Microsoft.Visio.Drawing";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.MainRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown ModelTargetDropDown;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton VerifyButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ImportButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ExportButton;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal MainRibbon MainRibbon
+        {
+            get { return this.GetRibbon<MainRibbon>(); }
+        }
+    }
+}
---
+++ VisioAddin2010/MainRibbon.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using SPES_Modelverifier_Base;
+using System.Windows.Forms;
+using ITU_Scenario;
+using SPES_Funktionsnetz;
+using NetOffice.VisioApi;
+
+namespace VisioAddin2010
+{
+    public partial class MainRibbon
+    {
+        List<ModelNetwork> modelverifiers = new List<ModelNetwork>();
+
+        ModelNetwork previousModelverifier = null;
+        ModelNetwork activeModelverifier => modelverifiers.FirstOrDefault(t => t.ToString() == this.ModelTargetDropDown.SelectedItem?.Label);
+        bool initialized = false;
+
+        private void MainRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+            //get current application
+            var application = NetOffice.VisioApi.Application.GetActiveInstance();
+
+            //add modelverifiers
+            modelverifiers.Add(new ScenarioNetwork(application));
+            modelverifiers.Add(new FunktionsnetzNetwork(application));
+
+            //add modelverifiers to dropdown menu and subscribe to events
+            foreach (var obj in modelverifiers)
+            {
+                //dropdown
+                var item = Globals.Factory.GetRibbonFactory().CreateRibbonDropDownItem();
+                item.Label = obj.ToString();
+                this.ModelTargetDropDown.Items.Add(item);
+
+                //sub to log messages etc.
+                obj.OnErrorReceivedEvent += delegate (Exception pEx) { System.Windows.Forms.MessageBox.Show(pEx.Message); };
+                //obj.OnLogMessageReceivedEvent += delegate (String pMessage) { System.Windows.Forms.MessageBox.Show(pMessage); };
+            }
+
+            //call selection changed for init shape load (only if document is loaded)
+            if (application.ActiveDocument != null)
+                ModelTargetDropDown_SelectionChanged(null, null);
+
+            //subscribe to application events
+            application.DocumentCreatedEvent += Application_DocumentLoadedOrCreated;
+            application.DocumentOpenedEvent += Application_DocumentLoadedOrCreated;
+        }
+
+        private void ModelTargetDropDown_SelectionChanged(object sender, RibbonControlEventArgs e)
+        {
+            if (previousModelverifier != null)
+                previousModelverifier.UnloadShapes();
+
+
+            activeModelverifier.LoadShapes();
+            previousModelverifier = activeModelverifier;
+        }
+
+        private void ExportButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.AddExtension = true;
+                dialog.DefaultExt = "xml";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.activeModelverifier.Export(dialog.FileName);
+                    MessageBox.Show("Export successful");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Export failed: " + ex.Message,
+                        "ERROR",
+                        System.Windows.Forms.MessageBoxButtons.OK,
+                        System.Windows.Forms.MessageBoxIcon.Error);
+            }
+        }
+
+        private void VerifyButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            if (activeModelverifier != null)
+            {
+                try
+                {
+                    this.activeModelverifier.Verify();
+                    System.Windows.Forms.MessageBox.Show("Verification successful!", "Success!", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
+                }
+                catch (ValidationFailedException ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(String.Format("Sheet: {0} \nShape: {1}\nMessage: {2}", ex.ExceptionObject.visiopage, ex.ExceptionObject.uniquename, ex.Message),
+                        "Validation failed!",
+                        System.Windows.Forms.MessageBoxButtons.OK,
+                        System.Windows.Forms.MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show("Verification failed: " + ex.Message,
+                        "ERROR",
+                        System.Windows.Forms.MessageBoxButtons.OK,
+                        System.Windows.Forms.MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void ImportButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.DefaultExt = "xml";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.activeModelverifier.Import(dialog.FileName);
+                    MessageBox.Show("Import successful");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Import failed: " + ex.Message,
+                        "ERROR",
+                        System.Windows.Forms.MessageBoxButtons.OK,
+                        System.Windows.Forms.MessageBoxIcon.Error);
+            }
+        }
+        private void Application_DocumentLoadedOrCreated(IVDocument pDoc)
+        {
+            if (!initialized)
+            {
+                ModelTargetDropDown_SelectionChanged(null, null);
+                initialized = true;
+            }
+        }
+    }
+}
