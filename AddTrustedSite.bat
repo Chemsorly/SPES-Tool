a00c7a31244958ad55fb95ce3bbb6f92d957a0f5
chemsorly <contact@chemsorly.com>
2018-01-08 09:26:03 +0100

    update trusted site script

--- AddTrustedSite.bat
+++ AddTrustedSite.bat
@@ -1,3 +1,3 @@
 REM See http://technet.microsoft.com/en-us/library/cc732643.aspx for Reg Commands/Switches
-REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\chemsorly.com\releases" /v "https" /t REG_DWORD /d 00000002
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\uni-due.de\paluno\spes-tool" /v "https" /t REG_DWORD /d 00000002
 pause
